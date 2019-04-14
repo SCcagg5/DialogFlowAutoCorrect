3655ad3716dd1dd8a96ce7f94580445d35e1b028
SCcagg5 <sccagg5@gmail.com>
2019-04-14 22:59:15 +0200

    exo creation

---
+++ back-end/src/exercice.py
@@ -0,0 +1,21 @@
+import requests
+import json as JSON
+import urllib
+
+class exercice_maker:
+    def csv_to_arr(self, csv):
+        
+
+
+
+
+    {
+    "fr": {
+	"queries" : [
+	    ["bonjour"]
+	],
+	"waited": [
+	    ["Bienvenue !", "Salutations !", "Salut !", "Bonjour !"]
+	]
+    }
+}
--- back-end/src/server.py
+++ back-end/src/server.py
@@ -3,6 +3,8 @@ import time
 from returnvalue import ret
 from params import check
 from dialogflow import dialogflowapi
+from exercice import exercice_maker
+import uuid
 
 """route to test if api is up"""
 @get('/test/')
@@ -18,9 +20,9 @@ def base():
 @post('/correct/')
 def base():
     try:
-                params = check.json(request)
+        params = check.json(request)
     except:
-                params = []
+        params = []
     toret = ret(request.route.rule, params)
 
     if not toret.err:
@@ -37,6 +39,31 @@ def base():
             toret.add_data(err[1])
 
     return toret.ret()
+
+@post('/add_exo/')
+def base():
+    toret = ret(request.route.rule, params)
+
+    upload = request.files.get('upload')
+    name, ext = os.path.splitext(upload.filename)
+    if ext not in ('.csv'):
+        toret.add_error("Invalid file extension '" + ext + "'" , 401)
+    toret.add_data(upload)
+    return toret.ret()
+
+    if not toret.err:
+        err = check.contain(params, ["mail", "name"])
+        if not err[0]:
+            toret.add_error(err[1], err[2])
+
+    if not toret.err:
+        df = dialogflowapi(params["bearer"])
+        err = df.test(params["exercice"])
+        if not err[0]:
+            toret.add_error(err[1], err[2])
+        else:
+            toret.add_data(err[1])
+    str(uuid.uuid4())
 
 
 if __name__ == '__main__':
