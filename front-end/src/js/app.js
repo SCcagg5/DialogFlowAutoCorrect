d7f643e85070ddfebeb8a1e9669ae9a9b7754504
SCcagg5 <sccagg5@gmail.com>
2019-05-10 10:45:47 +0200

    call app

--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -5,9 +5,13 @@ new Vue({
       success: null,
       lastexo: null,
       bearer: null,
+      exercice: null
     },
     methods: {
       correct: function(){
+        if (!this.bearer || !this.exercice)
+          return;
+        this.lastexo = this.exercice;
         this.ajaxRequest = true;
         data = {
           "bearer": this.bearer,
@@ -15,8 +19,28 @@ new Vue({
         }
         url = "http://localhost:5002/correct/"
         axios.post(url, data)
-             .then(response => {console.log(response)})
+             .then(response => {this.setup_return(response.data.data, response.data.data.succes)})
              .catch(error => console.log(error));
+      },
+      setup_return: function(response, suc) {
+        this.success= suc
+        delete response.succes
+        this.error["errorcode"] = "WGW"
+        this.error = JSON.stringify(response, undefined, 2).replace(/\n/g, '<br>');
+        this.error = "<pre>" +this.error+ "</pre>"
+        localStorage.history = true;
+        localStorage.bearer = this.bearer;
+        localStorage.success = this.success;
+        localStorage.error = this.error;
+        localStorage.exercice = this.exercice;
+      }
+    },
+    mounted(){
+      if(localStorage.history) {
+      this.bearer = localStorage.bearer;
+      this.success = JSON.parse(localStorage.success);
+      this.lastexo = localStorage.exercice;
+      this.error = localStorage.error
       }
     }
 })
